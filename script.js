c71a632190e24ad898dbf3d06243f145465c93d5
Maria Rosario <mariarosariojohansson@gmail.com>
2022-04-12 21:05:08 +0200

    Memory card game

---
+++ script.js
@@ -0,0 +1,35 @@
+const cards = document.querySelectorAll(".card");
+
+let flippedCard = false;
+let firstFlippedCard, secondFlippedCard;
+
+function pickCard() {
+  this.classList.add("flip");
+
+  if (!flippedCard) {
+    flippedCard = true;
+    firstFlippedCard = this;
+  } else {
+    flippedCard = false;
+    secondFlippedCard = this;
+
+    if (firstFlippedCard.dataset.game === secondFlippedCard.dataset.game) {
+      firstFlippedCard.removeEventListener("click", pickCard);
+      secondFlippedCard.removeEventListener("click", pickCard);
+    } else {
+      setTimeout(() => {
+        firstFlippedCard.classList.remove("flip");
+        secondFlippedCard.classList.remove("flip");
+      }, 1100);
+    }
+  }
+}
+
+(function shuffleBoard() {
+  cards.forEach((card) => {
+    let randomize = Math.floor(Math.random() * 12);
+    card.style.order = randomize;
+  });
+})();
+
+cards.forEach((card) => card.addEventListener("click", pickCard));
